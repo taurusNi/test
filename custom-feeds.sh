a4ce3646da5c0dff0d4c18860fef92b1a4894539
taurusni <taurus_ni@yahoo.com.au>
2025-02-18 21:26:18 +0800

    first commit

---
+++ custom-feeds.sh
@@ -0,0 +1,6 @@
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' $BUILD_ROOT/feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >> $BUILD_ROOT/feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> $BUILD_ROOT/feeds.conf.default
